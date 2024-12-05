ca8d4490554ba082df8a836295f5d5d228dcb51f
João Pedro <jppedroca1000@gmail.com>
2024-12-05 11:30:31 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,12 @@
+print('seu_nome')
+print('sua_idade')
+print('sua_altura')
+nome_usuario = 'João Pedro' # tipo -> str = string
+idade_usuario = 19 # tipo -> str = string 
+altura_usuario = 1.86 # tipo -> float = flutuante
+maior_idade = True # tipo -> bool = 
+print('seja Bem-Vindo', nome_usuario)
+print('sua idade é , idade_usuario, anos')
+print('você tem, altura_usuario')metros
+print("você é maior de idade?")
+ print('maior_idade = True')
